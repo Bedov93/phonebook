17dc978ddbb868708ae7b576980937668d7567ec
Denis Bedov <bedov.work@gmail.com>
2020-08-24 00:16:06 +0300

    PB-1 Login clear core model

--- public/application/core/model.php
+++ public/application/core/model.php
@@ -2,25 +2,5 @@
 
 class Model
 {
-    /**
-     * @param $query запрос
-     * @param $type тип возвращаемого значения
-     * @return array|bool
-     */
-    public function query($query,$type = MYSQLI_ASSOC)
-    {
-        return Database::query($query,$type);
-    }
 
-    public function exec($query){
-        return Database::exec($query);
-    }
-
-    public function fetchOne($query,$type = MYSQLI_ASSOC)
-    {
-        return Database::fetchOne($query,$type);
-    }
-    public function get_data()
-    {
-    }
 }
